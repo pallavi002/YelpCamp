ef493dd73319b2b48ab161721c91bc771eff489e
pallavi002 <pallavilodhi08@gmail.com>
2020-04-05 15:20:50 +0530

    comments added and show page ui improved

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const SeedDB = require('./seeds')
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 
 initDatabase();
 
@@ -32,36 +33,36 @@ SeedDB();
 //   })
 // });
 
-app.get('/', function(req, res, next) {
-  res.render('index');
+app.get('/', function (req, res, next) {
+  res.render('campgrounds/index');
 });
 
-app.get('/campgrounds', function(req, res) {
-  Campground.find({}, function(err, results) {
-    if(err) {
-      console.log('Error Occured'+ err);
+app.get('/campgrounds', function (req, res) {
+  Campground.find({}, function (err, results) {
+    if (err) {
+      console.log('Error Occured' + err);
     } else {
       console.log(results)
-      res.render('campgrounds.ejs', {
-        results : results
+      res.render('campgrounds/campgrounds.ejs', {
+        results: results
       });
     }
   });
-  
+
 })
 
-app.post('/campgrounds', function(req, res) {
+app.post('/campgrounds', function (req, res) {
   let name = req.body.name;
   let image = req.body.image;
   let description = req.body.description;
   let newCampground = {
     name: name,
-    description: description, 
+    description: description,
     image: image
   }
   // campgrounds.push(newCampground)
-  Campground.create(newCampground, function(err, res) {
-    if(err) {
+  Campground.create(newCampground, function (err, res) {
+    if (err) {
       console.log('Error occured while posting.');
     } else {
       console.log('Successfullt created');
@@ -70,23 +71,57 @@ app.post('/campgrounds', function(req, res) {
   res.redirect('/campgrounds');
 });
 
-app.get('/campgrounds/new', function(req, res) {
-  res.render('form.ejs');
+app.get('/campgrounds/new', function (req, res) {
+  res.render('campgrounds/form.ejs');
 })
 
-app.get('/campgrounds/:id', function(req, res) {
-  Campground.findById(req.params.id).populate('comments').exec(function(err, campDescription) {
-    if(err) {
-      console.log('Cannot find the description'+ err);
+app.get('/campgrounds/:id', function (req, res) {
+  Campground.findById(req.params.id).populate('comments').exec(function (err, campDescription) {
+    if (err) {
+      console.log('Cannot find the description' + err);
     } else {
       console.log(campDescription);
-      res.render('show', {
+      res.render('campgrounds/show', {
         campDescription: campDescription
       });
     }
   })
 })
 
-app.listen('3005', function() {
+//comments routes
+app.get('/campgrounds/:id/comments/new', function (req, res) {
+  Campground.findById(req.params.id, function (err, campground) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('comments/new.ejs', {
+        campground: campground
+      });
+
+    }
+  })
+});
+
+app.post('/campgrounds/:id/comments', function(req, res) {
+  Campground.findById(req.params.id, function(err, campground) {
+    if (err) {
+      console.log(err);
+      res.redirect('/campgrounds');
+    } else {
+      Comment.create(req.body.comment, function(err, comment) {
+        if (err) {
+          console.log(err);
+          res.redirect('/campgrounds');
+        } else {
+          campground.comments.push(comment);
+          campground.save();
+          res.redirect('/campgrounds/'+ campground._id);
+        }
+      })
+    }
+  })
+})
+
+app.listen('3005', function () {
   console.log('YelpCamp Server Running on port 3005...');
 });
--- seeds.js
+++ seeds.js
@@ -7,18 +7,18 @@ let data = [
   {
     name: 'Clouds rest',
     image: 'https://images.unsplash.com/photo-1565053396207-75ca17bdf99c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60',
-    description: 'The clouds are dark and resting in the sky, lorem iohi woijfow wooe fkor eiwi hit edkj dbidjo odsi ehifh jvoe,he clouds are dark and resting in the sky, lorem iohi woijfow wooe fkor eiwi hit edkj dbidjo odsi ehifh jhe clouds are dark and resting in the sky, lorem iohi woijfow wooe fkor eiwi hit edkj dbidjo odsi ehifh j'
+    description: 'The clouds are dark and resting in the sky,Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
   },
-  {
+  { 
     name: 'Desert Mesa',
     image: 'https://images.unsplash.com/photo-1477581265664-b1e27c6731a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1296&q=60',
-    description: 'The Desert mesa hdo9w ewov in the sky, lorem iohi woijfow wooe fkor eiwi hit edkj dbidjo odsi ehifh jvoe,he clouds are dark and resting in the sky, lorem iohi woijfow wooe fkor eiwi hit edkj dbidjo odsi ehifh jhe clouds are dark and resting in the sky, lorem iohi woijfow wooe fkor eiwi hit edkj dbidjo odsi ehifh j'
+    description: 'The clouds are dark and resting in the sky,Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
 
   },
   {
     name: 'Canyon floora',
     image: 'https://images.unsplash.com/photo-1523987355523-c7b5b0dd90a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=60',
-    description: 'The canyaon cjsojc fllora are dark and resting in the sky, lorem iohi woijfow wooe fkor eiwi hit edkj dbidjo odsi ehifh jvoe,he clouds are dark and resting in the sky, lorem iohi woijfow wooe fkor eiwi hit edkj dbidjo odsi ehifh jhe clouds are dark and resting in the sky, lorem iohi woijfow wooe fkor eiwi hit edkj dbidjo odsi ehifh j'
+    description: 'The clouds are dark and resting in the sky,Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
   }
 ]
 
